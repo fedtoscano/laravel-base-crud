90d4b5d304dcc19c97c52c1335b0293bbcd2f307
fedtoscano <federicotoscano611@gmail.com>
2024-07-25 17:25:14 +0200

    aggiornato il modello Animal con il tratto SoftDeletes

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Animal extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $table = "animals";
     protected $fillable = [
         "Nome",
